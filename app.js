0b0f1313aa56a79583f775ab6d8b92ca2106caeb
Kshitij Chauhan <ckshitij2@gmail.com>
2024-08-24 20:12:27 +0530

    This is changed

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
-// Define the API URL as a variable
 const API_URL = 'https://9lzyamugrl.execute-api.ap-south-1.amazonaws.com/prod/tasks'; // Replace with your actual API Gateway URL
 
 document.addEventListener('DOMContentLoaded', () => {
     const taskList = document.getElementById('taskList');
-    const taskForm = document.getElementById('taskForm');
-    const taskIdInput = document.getElementById('taskId');
-    const taskNameInput = document.getElementById('taskName');
-    const taskOwnerInput = document.getElementById('taskOwner');
 
     // Function to fetch and display tasks
     async function fetchTasks() {
@@ -68,37 +63,6 @@ document.addEventListener('DOMContentLoaded', () => {
             console.error('Error fetching tasks:', error);
         }
     }
-
-    // Function to add a new task
-    taskForm.addEventListener('submit', async (event) => {
-        event.preventDefault();
-        const taskId = taskIdInput.value;
-        const taskName = taskNameInput.value;
-        const taskOwner = taskOwnerInput.value;
-
-        try {
-            const response = await fetch(API_URL, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'x-api-key': 'X8TnD3EWqI8BskW1BuBxe2n1LeseuViOaE9a07VY'
-                },
-                body: JSON.stringify({ taskId, taskName, taskOwner })
-            });
-
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-
-            taskIdInput.value = '';
-            taskNameInput.value = '';
-            taskOwnerInput.value = '';
-
-            fetchTasks(); // Refresh the task list
-        } catch (error) {
-            console.error('Error adding task:', error);
-        }
-    });
 
     // Initial fetch
     fetchTasks();
